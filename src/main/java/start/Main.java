d1960c744b441eca3b83ea286af17f69a4c94a5e
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2024-06-05 18:59:45 +0200

    Update Main.java

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,13 +1,8 @@
 package start;
 
-//DELIO ERROSA da Git bash :)
-//Hellooo - Bruno <3
-//Hello World
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("First commit main");
-                System.out.println("Test Repository");
+	public static void main(String[] args) { 
 	}
 
 }
