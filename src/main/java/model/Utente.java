ba16cd436ab8a9934ea4f177b84717743dac436a
Antonio-Lamberti <lambertiantoniola@gmail.com>
2024-06-07 12:49:58 +0200

    Completati tutti i passaggi con commenti

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,16 +1,40 @@
 package model;
 
+import java.util.Objects;
+
+import com.google.gson.annotations.Expose;
+
 public class Utente {
-	private String nome;
+	@Expose								//@Expose usato per obbligare il suo inserimento in JSON
+	private String nome;		
+	@Expose
 	private String cognome;
+	@Expose
 	private String sesso;
+	private transient int eta;			//Keyword transient per non inserire questo attributo in JSON
 	
-	public Utente() {}
+	public Utente(String nome) {
+		this.nome = nome;
+	}
 	
 	public Utente(String nome, String cognome, String sesso) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.sesso = sesso;
+	}
+	
+	public Utente(String nome, String cognome, int eta) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.eta = eta;
+	}
+	
+	public Utente(String nome, String cognome, String sesso, int eta) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.sesso = sesso;
+		this.eta = eta;
 	}
 
 	public String getNome() {
@@ -21,23 +45,60 @@ public class Utente {
 		this.nome = nome;
 	}
 
+
+
 	public String getCognome() {
 		return cognome;
 	}
+
+
 
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
 
+
+
 	public String getSesso() {
 		return sesso;
 	}
 
+
+
 	public void setSesso(String sesso) {
 		this.sesso = sesso;
 	}
-	
-	public String toString() {
-		return "Utente [ " + nome + " " + cognome + " ";
+
+	public int getEta() {
+		return eta;
 	}
+	
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+	public String toString() {
+		return nome + " " + cognome + " " + sesso + " " + eta;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cognome, nome, sesso);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utente other = (Utente) obj;
+		return Objects.equals(cognome, other.cognome) && Objects.equals(nome, other.nome)
+				&& Objects.equals(sesso, other.sesso);
+	}
+	
+	
+	
 }
--- src/main/java/utility/StringUtility.java
+++ src/main/java/utility/StringUtility.java
@@ -1,27 +1,48 @@
 package utility;
 
+import java.util.ArrayList;
+import javax.xml.bind.JAXB;
+
+import com.google.gson.Gson;
+
 import model.Utente;
 
 public class StringUtility{
 	public static String toJson(){
-		
+		Utente u = new Utente("Antonio", "Lamberti", "M");		//Creare oggetto utente
+		Gson gson = new Gson();									
+		String json = gson.toJson(u);							//Conversione da Oggetto a Json
+		System.out.println(json + "\n");
 		return "";
 	}
 	
 	public static String fromJson() {
-		Utente u = new Utente();
-		
+		ArrayList <Utente> arr = new ArrayList<Utente>();
+		Utente uProprio;
+		String json1 = "{\"nome\":\"Antonio\", \"cognome\":\"Lamberti\", \"sesso\":\"M\"}";		//Presi i dati da JSON
+		String json2 = "{\"nome\":\"Gianluigi\", \"cognome\":\"Sperti\", \"sesso\":\"M\"}";
+		String json3 = "{\"nome\":\"Marco\", \"cognome\":\"Ferrante\", \"sesso\":\"M\"}";
+		Gson gson = new Gson();
+		Utente u1 = gson.fromJson(json1, Utente.class);											//Trasformare i dati da JSON a Oggetti in Java
+		Utente u2 = gson.fromJson(json2, Utente.class);
+		Utente u3 = gson.fromJson(json3, Utente.class);
+		arr.add(u1);																			//Inserire i dati da Oggetti in un Array
+		arr.add(u2);
+		arr.add(u3);
+		for(int i = 0; i<arr.size(); i++) {
+			if(arr.get(i).getNome().equals("Antonio")) {										//Individuare il nome proprio
+				uProprio = arr.get(i);
+				uProprio.setEta(25);															//Aggiungere l'età
+				JAXB.marshal(uProprio, System.out);												//Conversione da Array a XML con stampa
+			}
+		}
+		System.out.println();
 		return "";
 	}
 	
 	public static String toXML(){
-		
-		return "";
-	}
-	
-	public static String fromXML() {
-		Utente u = new Utente();
-		System.out.println("niente");
+		Utente u = new Utente("Antonio", "Lamberti", 25);										//Creazione Oggetto Java
+		JAXB.marshal(u, System.out);															//Conversione Oggetto Java in XML
 		return "";
 	}
 }
